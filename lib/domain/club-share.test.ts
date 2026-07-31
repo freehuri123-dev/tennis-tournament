8693cdc0341f935de053c5e0c17070e058a383b4
jhpark <freehuri123@gmail.com>
2026-07-31 10:29:02 +0900

    Refine Kakao share descriptions

--- lib/domain/club-share.test.ts
+++ lib/domain/club-share.test.ts
@@ -3,10 +3,10 @@ import { getClubShareContent } from "./club-share";
 
 describe("getClubShareContent", () => {
   it.each([
-    ["stc", "/kakao-share-stc.jpg", "STC\uC640 \uD568\uAED8\uD558\uB294 \uC990\uAC70\uC6B4 \uC2B9\uBD80, \uB300\uC9C4\uD45C\uC640 \uC21C\uC704\uB97C \uD655\uC778\uD558\uC138\uC694."],
-    ["otc", "/kakao-share-otc.jpg", "OTC\uC640 \uD568\uAED8\uD558\uB294 \uC990\uAC70\uC6B4 \uC2B9\uBD80, \uB300\uC9C4\uD45C\uC640 \uC21C\uC704\uB97C \uD655\uC778\uD558\uC138\uC694."],
-    ["joogo", "/kakao-share-joogo.jpg", "\uD568\uAED8\uD558\uB294 \uD14C\uB2C8\uC2A4, \uC990\uAC70\uC6B4 \uBAA8\uC784! \uB300\uC9C4\uD45C\uC640 \uC21C\uC704\uB97C \uD655\uC778\uD558\uC138\uC694."],
-    ["army", "/kakao-share-army.jpg", "\uCC9C\uD558\uC81C\uC77C1\uC0AC\uB2E8\uC758 \uB728\uAC70\uC6B4 \uC2B9\uBD80, \uB300\uC9C4\uD45C\uC640 \uC21C\uC704\uB97C \uD655\uC778\uD558\uC138\uC694."]
+    ["stc", "/kakao-share-stc.jpg", "STC \uACBD\uAE30 \uC77C\uC815\uACFC \uC2E4\uC2DC\uAC04 \uC21C\uC704\uB97C \uD655\uC778\uD558\uC138\uC694."],
+    ["otc", "/kakao-share-otc.jpg", "OTC \uACBD\uAE30 \uC77C\uC815\uACFC \uC2E4\uC2DC\uAC04 \uC21C\uC704\uB97C \uD655\uC778\uD558\uC138\uC694."],
+    ["joogo", "/kakao-share-joogo.jpg", "\uC8FC\uACE0\uBC1B\uACE0 \uACBD\uAE30 \uC77C\uC815\uACFC \uC2E4\uC2DC\uAC04 \uC21C\uC704\uB97C \uD655\uC778\uD558\uC138\uC694."],
+    ["army", "/kakao-share-army.jpg", "\uCC9C\uD558\uC81C\uC77C1\uC0AC\uB2E8 \uACBD\uAE30 \uC77C\uC815\uACFC \uC2E4\uC2DC\uAC04 \uC21C\uC704\uB97C \uD655\uC778\uD558\uC138\uC694."]
   ] as const)("returns club-specific content for %s", (clubSlug, imagePath, description) => {
     const content = getClubShareContent(clubSlug);
 
